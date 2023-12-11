b4526d7791844b8b792b374745aa1fe394af48ba
Brian Simpson <bsimpson63@gmail.com>
2023-12-11 13:41:13 -0500

    template

---
+++ TEMPLATE/solution.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+INPUT_SHORT = """""".strip()
+
+
+with open(Path(__file__).with_name("input.txt")) as f:
+    INPUT_LONG = f.read().strip()
+
+
+def part1():
+    pass
+
+
+def part2():
+    pass
+
+
+if __name__ == "__main__":
+    part1()
